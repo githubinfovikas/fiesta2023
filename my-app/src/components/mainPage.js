2790a53f36dd36dcd3ee7e2ebd4c5a25fa982574
githubinfovikas <vikasdbg453@gmail.com>
2023-08-02 20:09:46 +0530

    up

--- my-app/src/components/mainPage.js
+++ my-app/src/components/mainPage.js
@@ -34,7 +34,7 @@ const MainPage = () => {
                                 
                             </p>
                             <p style={{ color: 'white', fontSize: '2vh' }}>
-                                Registration last date 30 <sup>th</sup> JULY
+                                Registration last date 02 <sup>th</sup> AUGUST
                             </p>
 
                             <a href='/userId-reg' className='btn btn-primary hower mx-auto m-2' style={{ width: '30vh' }} >Register </a>
